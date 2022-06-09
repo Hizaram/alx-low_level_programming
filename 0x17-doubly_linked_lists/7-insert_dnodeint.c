74a61cbc2ff0f270331392af620b74896d9079c7
Hizaram <promisevector@gmail.com>
2022-06-09 19:27:32 +0100

    Made provision for addition of nodes at the start and end of the list(corrections)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include "2-add_dnodeint.c"
-#include "3-add_dnodeint_end.c"
 
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list
@@ -40,11 +38,55 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
-		return (add_dnodeint(h, n));
+	{
+		dlistint_t *new;
+		
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->prev = NULL;
+		
+		if (*h == NULL)
+		{
+			new->next = NULL;
+			*h = new;
+			return (*h);
+		}
+		
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (*h);
+	}
 
 	len = dlistint_len(*h);
 	if (idx == len - 1)
-		return (add_dnodeint_end(h, n));
+	{
+		dlistint_t *new, *temp;
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		
+		new->n = n;
+		new->next = NULL;
+		
+		if (*h == NULL)
+		{
+			new->prev = NULL;
+			*h = new;
+			return (*h);
+		}
+		
+		temp = *h;
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+		new->prev = temp;
+		return (new);
+	}
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
